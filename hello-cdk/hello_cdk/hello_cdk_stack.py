29c1574fb3baa6b924f758ff49a538d1bab5d5b1
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2024-04-13 18:44:17 +0000

    working first iac

--- hello-cdk/hello_cdk/hello_cdk_stack.py
+++ hello-cdk/hello_cdk/hello_cdk_stack.py
@@ -1,12 +1,24 @@
 import aws_cdk as cdk
 from constructs import Construct
-import aws_cdk.aws_s3 as s3
-
+from aws_cdk import (
+    aws_dynamodb as dynamodb,
+    aws_s3 as s3,
+    aws_s3_notifications as s3Notifications,
+)
 
 class HelloCdkStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        dynamodb_table = cdk.aws_dynamodb.TableV2(
+            self,
+            "MyTable",
+            partition_key=dynamodb.Attribute(
+                name="Id", type=dynamodb.AttributeType.NUMBER
+            ),
+            billing=dynamodb.Billing.on_demand(),
+        )
 
         hello_world_function = cdk.aws_lambda.Function(
             self,
@@ -14,12 +26,16 @@ class HelloCdkStack(cdk.Stack):
             runtime=cdk.aws_lambda.Runtime.PYTHON_3_12,
             code=cdk.aws_lambda.Code.from_asset("lambda"),
             handler="hello.lambda_handler",
+            environment={"DYNAMO_DB_TABLE_NAME": dynamodb_table.table_name},
         )
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "HelloCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(
+            self,
+            "MyBucket",
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+        bucket.add_event_notification(
+            s3.EventType.OBJECT_CREATED,
+            s3Notifications.LambdaDestination(hello_world_function),  # type: ignore
+        )
